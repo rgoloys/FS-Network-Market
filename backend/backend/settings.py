d9d245451c40cd6cad2eac0f76eb00998753651e
rgoloys <rgoloys@users.noreply.github.com>
2026-06-06 11:34:06 +0800

    allowed host

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -38,7 +38,7 @@ SECRET_KEY = os.getenv('DJANGO_SECRET_KEY', 'django-insecure-change-me')
 DEBUG = os.getenv('DJANGO_DEBUG', 'False').lower() == 'true'
 
 #ALLOWED_HOSTS = env_list('DJANGO_ALLOWED_HOSTS', 'localhost,127.0.0.1')
-ALLOWED_HOSTS = env_list["*"]
+ALLOWED_HOSTS = ["*"]
 
 
 # Application definition
