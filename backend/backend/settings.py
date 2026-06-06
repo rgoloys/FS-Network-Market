3bb99272ccb60157914fc375134b044dbdea3cf1
rgoloys <rgoloys@users.noreply.github.com>
2026-06-06 13:31:32 +0800

    connecting domains

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -69,6 +69,9 @@ MIDDLEWARE = [
 CORS_ALLOWED_ORIGINS = env_list(
     'CORS_ALLOWED_ORIGINS',
     'http://localhost:5173,http://127.0.0.1:5173',
+    "https://fs-network-market-i1ph58e93-goloya-s-projects.vercel.app",
+    "https://fs-network-market-git-main-goloya-s-projects.vercel.app",
+    "https://fs-network-market.vercel.app",
 )
 
 ROOT_URLCONF = 'backend.urls'
--- frontend/src/api/base.js
+++ frontend/src/api/base.js
@@ -1,6 +1,8 @@
-const configuredBaseUrl =
+/*const configuredBaseUrl =
   import.meta.env.VITE_API_BASE_URL ?? "http://127.0.0.1:8000/";
 
 export const BASE_URL = configuredBaseUrl.endsWith("/")
   ? configuredBaseUrl
   : `${configuredBaseUrl}/`;
+  */
+export const BASE_URL = "https://asdb-isg.onrender.com/";
